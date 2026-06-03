81095306e4cd0e5b0e5ea5dd692e64d963fc81ac
hjkim0905 <eax9952@gmail.com>
2026-06-03 20:01:52 +0900

    feat(CORS): add localhost port 5173 to allow origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,8 +4,13 @@ import cartRouter from './routes/cart';
 
 const app = express();
 
+const ALLOWED_ORIGINS = ['http://localhost:3000', 'http://localhost:5173'];
+
 app.use((req: Request, res: Response, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  const origin = req.headers.origin;
+  if (origin && ALLOWED_ORIGINS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   if (req.method === 'OPTIONS') {
