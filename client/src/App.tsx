b33e0aeec3e9228cbc43325fb094bbae7614ac07
hjkim0905 <eax9952@gmail.com>
2026-06-05 15:54:01 +0900

    fix(client): update routing paths for github pages      compatibility

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
-import Cart from './pages/Cart';
-import ConfirmOrder from './pages/ConfirmOrder';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cart from "./pages/Cart";
+import ConfirmOrder from "./pages/ConfirmOrder";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter basename="/shopping-cart-full-stack">
       <Routes>
-        <Route path="/shopping-cart" element={<Cart />} />
-        <Route path="/shopping-cart/confirm" element={<ConfirmOrder />} />
+        <Route path="/" element={<Cart />} />
+        <Route path="/confirm" element={<ConfirmOrder />} />
       </Routes>
     </BrowserRouter>
   );
